481987c8c751322acdf0956f0792925eb804ce54
Sarvesh <142646866+sarvesh-official@users.noreply.github.com>
2025-04-22 21:58:56 +0530

    Bug Fix

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,7 +1,7 @@
-import SupaAuthVerifyEmail from "@/emails";
 import supabaseAdmin from "@/lib/supabase/admin";
 
 import { Resend } from "resend";
+import SupaAuthVerifyEmail from "../../../../emails";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {

1192ef7994710c269ce8c1bcbbd8044d5b878a1c
Sarvesh <142646866+sarvesh-official@users.noreply.github.com>
2025-04-22 22:03:50 +0530

    Implemented Auth and Basic Notes Ui

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -16,18 +16,14 @@ export function createSupabaseServer() {
 					try {
 						cookieStore.set({ name, value, ...options });
 					} catch (error) {
-						// The `set` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
+						console.log(error)
 					}
 				},
 				remove(name: string, options: CookieOptions) {
 					try {
 						cookieStore.set({ name, value: "", ...options });
 					} catch (error) {
-						// The `delete` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
+						console.log(error)
 					}
 				},
 			},
